8fb5a53f283e34ffb45df781e1a0402fd08a9517
Luke Lelias <llelias@e1z2r7p42.42.us.org>
2018-11-13 17:40:32 -0800

    putnbr works and normed

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,13 +6,30 @@
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 18:59:37 by llelias           #+#    #+#             */
-/*   Updated: 2018/11/12 16:03:36 by llelias          ###   ########.fr       */
+/*   Updated: 2018/11/13 17:36:26 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_putnbr(int n)
+static void	ft_putnbr_rec(long n)
 {
-	ft_putstr(ft_itoa(n));
+	if (n < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr_rec(n * -1);
+	}
+	else if (n)
+	{
+		ft_putnbr_rec(n / 10);
+		ft_putchar(ft_itoc((int)(n % 10)));
+	}
+}
+
+void		ft_putnbr(int n)
+{
+	if (n == 0)
+		ft_putchar('0');
+	else
+		ft_putnbr_rec((long)n);
 }
