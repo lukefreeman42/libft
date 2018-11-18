fc01c231cefd2f07aa101b5a320c480e2f79323e
Luke Lelias <llelias@e1z2r5p11.42.us.org>
2018-11-17 18:47:26 -0800

    memmove fix

--- ft_memrcpy.c
+++ ft_memrcpy.c
@@ -6,7 +6,7 @@
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 13:30:07 by llelias           #+#    #+#             */
-/*   Updated: 2018/11/13 20:11:54 by llelias          ###   ########.fr       */
+/*   Updated: 2018/11/17 18:47:11 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,12 @@
 
 void	*ft_memrcpy(void *dst, const void *src, size_t n)
 {
-	void *p;
+	void *p;	
 
-	if (dst && src)
-	{
-		p = dst;
-		dst += n - 1;
-		src += n - 1;
-		while (n--)
-			*(uint8_t*)dst-- = *(uint8_t*)src--;
-		return (p);
-	}
-	else
-		return (NULL);
+	p = dst;
+	dst += n - 1;
+	src += n - 1;
+	while (n--)
+		*(uint8_t*)dst-- = *(uint8_t*)src--;
+	return (p);
 }
