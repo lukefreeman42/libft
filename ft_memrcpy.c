cf53369dc761fc04dcea1dab15893a482eef6c7b
Luke Lelias <llelias@e1z2r5p11.42.us.org>
2018-11-17 18:51:28 -0800

    normed

--- ft_memrcpy.c
+++ ft_memrcpy.c
@@ -6,7 +6,7 @@
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 13:30:07 by llelias           #+#    #+#             */
-/*   Updated: 2018/11/17 18:47:11 by llelias          ###   ########.fr       */
+/*   Updated: 2018/11/17 18:51:11 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memrcpy(void *dst, const void *src, size_t n)
 {
-	void *p;	
+	void *p;
 
 	p = dst;
 	dst += n - 1;
