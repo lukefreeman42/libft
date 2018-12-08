71ac36f388297209e3c878515289df0146d69c7d
lukefreeman42 <lukefreeman@protonmail.com>
2018-12-08 14:06:08 -0800

    changed freeing func param to **s

--- ft_fstrdup.c
+++ ft_fstrdup.c
@@ -6,20 +6,20 @@
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 08:20:51 by llelias           #+#    #+#             */
-/*   Updated: 2018/12/03 19:41:45 by llelias          ###   ########.fr       */
+/*   Updated: 2018/12/08 13:57:08 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_fstrdup(char *s)
+char	*ft_fstrdup(char **s)
 {
 	char *dup;
 
-	if (s)
-		if ((dup = ft_strdup(s)))
+	if (*s)
+		if ((dup = ft_strdup(*s)))
 		{
-			ft_strdel(&s);
+			ft_strdel(s);
 			return (dup);
 		}
 	return (NULL);
