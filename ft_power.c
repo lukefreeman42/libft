7352b094dbcae1726fc0b136e407c969fc83a5ed
lukefreeman42 <lukefreeman@protonmail.com>
2018-12-21 19:52:36 -0800

    normed ft_power

--- ft_power.c
+++ ft_power.c
@@ -6,25 +6,25 @@
 /*   By: llelias <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 10:19:22 by llelias           #+#    #+#             */
-/*   Updated: 2018/12/15 10:24:10 by llelias          ###   ########.fr       */
+/*   Updated: 2018/12/21 19:52:18 by llelias          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	static error(void)
+static int	error(void)
 {
 	ft_putstr("Does not support negative exponents... yet");
 	return (-1);
 }
 
-int		ft_power(int n, int p)
+int			ft_power(int n, int p)
 {
 	int tot;
 
 	tot = 1;
 	if (p < 0)
-		return(error());
+		return (error());
 	while (p-- > 0)
 		tot *= n;
 	return (tot);
